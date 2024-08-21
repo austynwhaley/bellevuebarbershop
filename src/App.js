e64e6b76dc342b1daa44c7c4c273cff6ffdd834c
Austyn Whaley <austyn_whaley@yahoo.com>
2024-08-21 01:09:21 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import barberPole from './images/barbpole.gif'
+
+function App() {
+  return (
+    <div className="window">
+      <div className="centered-content">
+        <img src={barberPole} alt="Left Icon" className="side-image flipped-horizontal" />
+        <h1>Bellevue Barbershop</h1>
+        <img src={barberPole} alt="Right Icon" className="side-image" />
+      </div>
+      <div className="window-content centered-content">
+        <p>Shorter hair or money back guaranteed!</p>
+      </div>
+      <div className="window-content centered-content">
+      <button className="button">Book Appointment</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
