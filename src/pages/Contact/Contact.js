67bcf46eaab1693e2cc57704537d011e1581430b
Austyn Whaley <austyn_whaley@yahoo.com>
2024-08-22 18:43:06 -0400

    contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,14 +1,24 @@
-import './Contact.css';
+import styles from './Contact.module.css';
+import storeFront from '../../images/storefront.jpg'
 
 function Contact() {
   return (
-    <div className="window">
-      <div className="centered-content">
-        <h1 className='shopHeader'>Contact!!</h1>
+    <div className={styles.window}>
+      <div className={styles['centered-content']}>
+        <h1 className={styles.contactHeader}>Contact Us</h1>
+        <div className={`${styles['window-content']} ${styles['centered-content']}`}>
+          <p>Store Hours</p>
+          <p>Tue-Fri 9am-6pm </p>
+          <p>Sat 9am-3pm</p>
+        </div>
+        <img src={storeFront} alt="storeFront" className={styles['contact-image']} />
       </div>
-
-      <div className="window-content centered-content">
-        <p>* Contact text *</p>
+      <div className={`${styles['window-content']} ${styles['centered-content']} ${styles['contact-info']}`}>
+        <p>Travis Ayers</p>
+        <p>400 Taylor Avenue</p>
+        <p>41073 Bellevue, Kentucky</p>
+        <p>bellevuebarbershop.ky@gmail.com</p>
+        <p>(859) 360-1911</p>
       </div>
     </div>
   );
